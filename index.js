b5751c7ca48c1a1e19a5dfe594f60183b65e2cc7
Asitha Sandaru <govinnagesandaru@gmail.com>
2025-04-20 11:36:10 +0300

    Database_Connection

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
-const path = require('path'); // need this mdoule in express the read the file
-const app = express();
-const exphbs = require('express-handlebars'); // import handle bars
-const localport = 8000;
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+require('dotenv').config();
 
+const app = express();
+
+// With this middleware we can get the data from HTML form
+app.use(express.urlencoded({extended: false}));
+
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars'); 
+
+
+const dbURI ='mongodb+srv://'+process.env.DBUSERNAME+':'+process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'+process.env.DB+'?retryWrites=true&w=majority&appName=Cluster0';
+//console.log(dbURI);
+
+
+mongoose.connect(dbURI)
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => console.log('Server is running on port ' + PORT));
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to DB:', err);
+  });
