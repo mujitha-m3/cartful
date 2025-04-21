2b7354d76777d8e5fd61489d3db3cebf1113a377
Asitha Sandaru <govinnagesandaru@gmail.com>
2025-04-21 10:12:37 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,39 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  full_name: String,
-  role: { type: String, default: 'customer' }
-});
+    fullName :
+    {
+        type : String,required: true,trim:true
+    },
+    email: 
+    {
+        type:String, required:true, lowercase:true,trim:true
+    },
+    password: {
+        type: String,required: true
+      },
+    gender: {
+        type :String
+    },
+ 
+  dateOfBirth: {type :Date},
+  language: {type:String},
+  phone: {type:String, trim:true},
+  profileImage: {type:String},
+  addressLine1: {type:String},
+  addressLine2: {type:String},
+  city:{type:String},
+  postalCode:{type:String},
+  country:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Country',
+    required: true
+  },
+  roles: {
+type: [String],
+    enum: ['user', 'admin'],default: ['user']},
+isActive: {
+    type: Boolean,default: true},
+})
 
-module.exports = mongoose.model('User', userSchema);
+module.exports= mongoose.model('User',userSchema);
