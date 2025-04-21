3bd4ec02b6d8439aec833ab5d9c760fcd877f73a
kcdissanayaka <kc.dissanayaka@outlook.com>
2025-04-21 13:15:12 +0300

    Cleaned up userController.js and resolved duplicate function conflict

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,41 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); // import user collection
 
-// Create User Function
-const registerNewUser = async (req,res) => {
-    
-    const {
-        fullName,email,password,
-        gender,dateOfBirth,language,
-        phone,profileImage,addressLine1,
-        addressLine2,city,postalCode,country
-      } = req.body;
-
-
-    const newUser = new User(
-        {
-            fullName,email,password,
-        gender,dateOfBirth,language,
-        phone,profileImage,addressLine1,
-        addressLine2,city,postalCode,country
-    }); 
-    try{
-        await newUser.save();
-        res.location(`http://localhost:8000/api/customerRegister/${newUser._id}`);
-        res.status(201).json(newUser);
-
-    }
-    catch (err) {
-        console.error(' Error in Customer Registration:', err);
-        res.status(500).send(' Registration Failed');
-    }
-}
-
-module.exports = {registerNewUser};
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-
 // POST: Register Customer
 const registerCustomer = async (req, res) => {
     try {
@@ -82,13 +47,8 @@ const registerCustomer = async (req, res) => {
     }
 };
 
-// Login 
-const loginCustomer = async (req, res) => {
-    // loging logic here
-};
-
 // Export as object
 module.exports = {
     registerCustomer
-   // loginCustomer
+    // loginCustomer
 };
